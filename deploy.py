9f6fd9e42e6980e6a5cbe335b331bc1a8be316f1
nickthelegend <testingtesla7@gmail.com>
2024-12-25 10:56:21 +0400

    Added get_admin and set_Admin and Deployed also please cross check it and verify

--- deploy.py
+++ deploy.py
@@ -1,5 +1,5 @@
 from beaker import client, sandbox
-from vaccine import app, get_app_state_val, set_app_state_val
+from vaccine import app, get_app_state_val, set_app_state_val, set_admin
 
 app.build().export("./artifacts")
 
@@ -13,6 +13,8 @@ app_client = client.ApplicationClient(
     signer=sender.signer,
 )
 
+
+print(f"The Admin is {sender.address}")
 app_id, addr, txid = app_client.create()
 
 print("App ID: ", app_id)
@@ -21,6 +23,13 @@ try:
     app_client.call(set_app_state_val, v=1)
 except:
     print("Failed as expected since this state is static")
+
+
+# try:
+app_client.call(set_admin, new_admin=sender.address)
+# except:
+print("Failed as expected since this state is static")
+
 
 result = app_client.call(get_app_state_val)
 print("App State: ", result.return_value)
--- vaccine.py
+++ vaccine.py
@@ -7,6 +7,16 @@ class GlobalState:
         stack_type=TealType.uint64,
         default=Int(0),
         static=False,
+        descr="An integer global state value"
+
+    )
+
+    AdminAddress = GlobalStateValue(
+        stack_type=TealType.bytes,
+        default=Bytes(""),
+        static=False,
+        descr="A Address which is a global state value"
+
     )
 app = Application("VaccineDistribution", state=GlobalState()).apply(
     unconditional_create_approval, initialize_global_state=True
@@ -21,3 +31,13 @@ def set_app_state_val(v: abi.Uint32) -> Expr:
 @app.external(read_only=True)
 def get_app_state_val(*, output: abi.Uint32) -> Expr:
     return output.set(app.state.TotalVaccines)
+
+
+@app.external
+def set_admin(new_admin: abi.Address):
+    return app.state.AdminAddress.set(new_admin.get())
+
+@app.external(read_only=True)
+def get_admin(*, output: abi.Address) -> Expr:
+    return output.set(app.state.AdminAddress.get())
+
